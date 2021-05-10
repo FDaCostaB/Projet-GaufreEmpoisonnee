77470bafb6ebcf6b0811a55f90c0b390a1efef55
root <root@PC.localdomain>
2021-05-10 07:34:35 +0200

    maj IA aleatoir

--- src/Controleur/IAAleatoire.java
+++ src/Controleur/IAAleatoire.java
@@ -14,15 +14,24 @@ class IAAleatoire extends IA {
         this.j=j;
     }
 
-    Coup coupIA(Jeu j) {
-        int l;
-        int c;
-        Coup cp ;
-
-        l = r.nextInt(j.longueur());
-        c = r.nextInt(j.largeur());
-        cp = new Coup(l,c);
-
-        return cp;
+    Coup coupIA(Jeu jeu) {
+    	int i, j;
+        int count = 0;
+        i = r.nextInt(jeu.getHeight());
+        j = r.nextInt(jeu.getWidth());
+        // l'IA essaye de ne pas se suicider et de manger quelque chose
+        while (!jeu.coupValide(new Coup(i,j)) && count < 100 || (i<=1 && j<=1)) {
+            i = r.nextInt(jeu.getHeight());
+            j = r.nextInt(jeu.getWidth());
+            count++;
+        }
+        if (count == 100) {
+        	return new Coup(0,0); //si elle n'as rien trouvée elle se suicide
+        }
+        //plateau.manger(new Couple(i,j));
+		if(jeu.grille()[i][j] == false)
+			return new Coup(i,j);
+		else 
+			return new Coup(0,0);
     }
 }
